353d76d29b75f46acd2c2f796b99733c46477236
peinnagato <37422960+psvnagasai@users.noreply.github.com>
2020-07-04 23:00:54 +0530

    gitignore and updated some stuff

--- server.js
+++ server.js
@@ -7,13 +7,20 @@ const http = require('http');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(morgan('dev'));
+app.use(express.static('./public')); //For the images, stylesheets, etc. THE STATIC FILES
+app.set('view engine', 'ejs'); //Setting up template engine, FOR THE FRONTEND
+
 
 app.get('/', function (req, res) {
    res.send('Hello World');
-})
+});
+
+app.get('/index', function(req, res){
+   res.render('index');
+});
 
 const server = http.createServer(app);
-require('./app/routes')(app);
+// require('./app/routes')(app);
 app.listen(8081, function () {
    
    console.log("Example app listening ")
